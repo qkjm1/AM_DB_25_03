88bc97f429242c61f87aa02a6f93bbe7f898b68d
qkjm1 <kqk9269@gmail.com>
2025-03-28 14:52:18 +0900

    DBMS Insert 까지

---
+++ src/main/java/org/example/JDBCConnTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class JDBCConnTest {
+    public static void main(String[] args) {
+        Connection conn = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            String url = "jdbc:mariadb://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+            conn = DriverManager.getConnection(url, "root", "");
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("연결 성공!");
+
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("드라이버 로딩 실패" + e);
+        } catch (SQLException e) {
+            System.out.println("에러 : " + e);
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Connection con = null;
+
         System.out.println("==프로그램 시작==");
 
         Scanner sc = new Scanner(System.in);
@@ -21,6 +26,7 @@ public class Main {
             if (cmd.equals("exit")) {
                 break;
             }
+
             if (cmd.equals("article write")) {
                 System.out.println("==글쓰기==");
                 int id = lastArticleId + 1;
@@ -34,7 +40,58 @@ public class Main {
 
                 lastArticleId++;
                 System.out.println(article);
+                System.out.println(id + "번 글이 작성되었습니다");
+
+                /// ///////////////////////////////////////////
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "insert into article";
+                    sql += " set regDate = now(),";
+                    sql += "updateDate = now(),";
+                    sql += "title = '" + title + "',";
+                    sql += "`body` = '" + body + "';";
+
+                    System.out.println(sql);
+
+                    pstmt = conn.prepareStatement(sql);
+
+                    int affectedRows = pstmt.executeUpdate();
+
+                    System.out.println("affected rows: " + affectedRows);
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             } else if (cmd.equals("article list")) {
+                String sql = "select * from article";
+
+                System.out.println(sql);
+
                 System.out.println("==목록==");
                 if (articles.size() == 0) {
                     System.out.println("게시글 없음");
@@ -49,5 +106,8 @@ public class Main {
 
         System.out.println("==프로그램 종료==");
         sc.close();
+
+
+
     }
 }
